ae0d80046189892e693cc65cf4ec16a5d73c6a21
abhisheks7070 <163820357+abhisheks7070@users.noreply.github.com>
2024-04-11 12:33:43 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
- mongoose.connect("mongodb+srv://sabhishek7070:Ttorres9@cluster0.3githov.mongodb.net/Payment-app")  //put your own mongoDB database URL
+ mongoose.connect("")  //put your own mongoDB database URL
 
 
 const userSchema = new mongoose.Schema({
