6933a32aac2fbb1a9bb6c7e0db1174131400571e
Abhishek <s.abhishek7070@gmail.com>
2025-01-27 12:24:18 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-const corsOptions = {
-    // origin: "http://localhost:5173" // frontend URI (ReactJS)
-    origin: "https://mywallet-ny2d.onrender.com" // frontend URI (ReactJS)
-}
+// const corsOptions = {
+//     // origin: "http://localhost:5173" // frontend URI (ReactJS)
+//     origin: "https://mywallet-ny2d.onrender.com" // frontend URI (ReactJS)
+// }
 
 app.use(cors());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
